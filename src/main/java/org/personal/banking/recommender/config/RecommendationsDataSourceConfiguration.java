95ad3c8d50f0989fb517c10bf9e1c790d2370c70
mariaaraslanova <maraslanowa@yandex.ru>
2026-01-27 15:42:17 +0300

    feat: Настроила подключение к базе данных H2 и зависимости проекта

--- src/main/java/org/personal/banking/recommender/config/RecommendationsDataSourceConfiguration.java
+++ src/main/java/org/personal/banking/recommender/config/RecommendationsDataSourceConfiguration.java
@@ -1,4 +1,31 @@
 package org.personal.banking.recommender.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
 public class RecommendationsDataSourceConfiguration {
+
+    @Bean(name = "recommendationsDataSource")
+    public DataSource recommendationsDataSource(@Value("${application.recommendations-db.url}") String recommendationsUrl) {
+        var dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(recommendationsUrl);
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setReadOnly(true);
+        return dataSource;
+    }
+
+    @Bean(name = "recommendationsJdbcTemplate")
+    public JdbcTemplate recommendationsJdbcTemplate(
+            @Qualifier("recommendationsDataSource") DataSource dataSource
+    ) {
+        return new JdbcTemplate(dataSource);
+    }
 }
+
+
