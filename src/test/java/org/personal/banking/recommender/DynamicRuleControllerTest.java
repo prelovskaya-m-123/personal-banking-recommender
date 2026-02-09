25a52483914b4b6c23a0aaa6e02e6b2bdf4b16e3
Himawari Lad <Sacrificial.animal@gmail.com>
2026-02-09 16:33:03 +0300

    coding-2.0-commit-RuleController-and-RuleRepository-tests

---
+++ src/test/java/org/personal/banking/recommender/DynamicRuleControllerTest.java
@@ -0,0 +1,100 @@
+package org.personal.banking.recommender;
+
+import org.junit.jupiter.api.Test;
+import org.personal.banking.recommender.controllers.DynamicRuleController;
+import org.personal.banking.recommender.entities.DynamicRule;
+import org.personal.banking.recommender.service.DynamicRuleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(DynamicRuleController.class)
+@AutoConfigureMockMvc
+public class DynamicRuleControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private DynamicRuleService dynamicRuleService;
+
+    /// CREATE DYNAMIC RULE ///
+    @Test
+    void shouldCreateDynamicRule() throws Exception {
+        String requestJson = """
+    {
+      "product_name": "Простой кредит",
+      "product_id": "ab138afb-f3ba-4a93-b74f-0fcee86d447f",
+      "product_text": "Текст рекомендации",
+      "rule": [
+        {
+          "query": "USER_OF",
+          "arguments": ["CREDIT"],
+          "negate": true
+        }
+      ]
+    }
+    """;
+
+        DynamicRule savedRule = new DynamicRule();
+        savedRule.setId(UUID.randomUUID());
+        savedRule.setProductName("Простой кредит");
+        savedRule.setProductId(UUID.fromString(
+                "ab138afb-f3ba-4a93-b74f-0fcee86d447f"));
+        savedRule.setProductText("Текст рекомендации");
+
+        when(dynamicRuleService.createRule(any(DynamicRule.class)))
+                .thenReturn(savedRule);
+
+        mockMvc.perform(post("/rule")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.product_name")
+                        .value("Простой кредит"));
+    }
+
+    /// GET ALL DYNAMIC RULES ///
+    @Test
+    void shouldReturnAllDynamicRules() throws Exception {
+        when(dynamicRuleService.getAllRules())
+                .thenReturn(List.of(
+                        new DynamicRule(),
+                        new DynamicRule()
+                ));
+
+        mockMvc.perform(get("/rule"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").isArray())
+                .andExpect(jsonPath("$.data.length()").value(2));
+    }
+
+    /// DELETE RULE ///
+    @Test
+    void shouldDeleteDynamicRule() throws Exception {
+        UUID id = UUID.randomUUID();
+
+        doNothing().when(dynamicRuleService).deleteRule(id);
+
+        mockMvc.perform(delete("/rule/{id}", id))
+                .andExpect(status().isNoContent());
+
+        verify(dynamicRuleService).deleteRule(id);
+    }
+
+
+}
---
+++ src/test/java/org/personal/banking/recommender/DynamicRuleRepositoryTest.java
@@ -0,0 +1,56 @@
+package org.personal.banking.recommender;
+
+import org.junit.jupiter.api.Test;
+import org.personal.banking.recommender.entities.DynamicRule;
+import org.personal.banking.recommender.postgres.repository.DynamicRuleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class DynamicRuleRepositoryTest {
+
+    @Autowired
+    private DynamicRuleRepository ruleRepository;
+
+    private DynamicRule testDynamicRule() {
+        DynamicRule rule = new DynamicRule();
+
+        rule.setProductName("Простой кредит");
+        rule.setProductId(UUID.randomUUID());
+        rule.setProductText("Текст рекомендации");
+
+        rule.setConditions(List.of(
+                new DynamicRule.RuleCondition(
+                        "USER_OF",
+                        List.of("CREDIT"),
+                        true
+                )
+        ));
+        return rule;
+    }
+
+    @Test
+    void shouldSaveAndFindRule() {
+        DynamicRule rule = testDynamicRule();
+
+        DynamicRule saved = ruleRepository.save(rule);
+
+        assertThat(saved.getId()).isNotNull();
+
+        List<DynamicRule> all = ruleRepository.findAll();
+
+        assertThat(all).hasSize(1);
+        assertThat(all.get(0).getProductName())
+                .isEqualTo("Простой кредит");
+    }
+
+}
